ea0bf79b2c8a3ea61c46aed3e751f4cc9c1638a1
bluez44 <vlqvinh444@gmail.com>
2025-03-22 11:43:06 +0700

    add teacher-create-quizz

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image'
 
 function Header() {
   return (
-    <div className='bg-white sticky top-0 w-full'>
+    <div className='bg-white fixed top-0 w-full'>
         <nav className='container mx-auto flex justify-between items-center py-4'>
             <div className='flex-1'>
                 <Image src={Logo} alt="Logo" width={100} height={100} />
             </div>
-            <ul className='flex justify-between items-center flex-1 gap-8 text-[#E0E0E0]'>
+            <ul className='flex justify-between items-center flex-1 gap-8 text-black'>
                 <li className='flex items-center'><a className='inline-block w-full p-4' href="#">How it work</a></li>
                 <li className='flex items-center'><a className='inline-block w-full p-4' href="#">Features</a></li>
                 <li className='flex items-center'><a className='inline-block w-full p-4' href="#">About us</a></li>
--- frontend/app/student/[id]/page.tsx
+++ frontend/app/student/[id]/page.tsx
@@ -2,9 +2,9 @@
 function Home() {
   return (
     <div className="bg-[#2faffe] min-h-lvh flex justify-center items-center">
-      <form action="" className="flex flex-col">
-        <label htmlFor="quizz_code">Quizz code</label>
-        <input type="text" placeholder="Enter your quizz code" className="py-2 px-4 border rounded focus:outline-[#0ffc8e]"/>
+      <form action="" className="flex flex-col gap-2 p-4 bg-white rounded">
+        <input type="text" placeholder="Enter your quizz code" className="py-2 px-4 border text-xl rounded focus:outline-[#0ffc8e]"/>
+        <button type="submit" className="bg-[#31f7c4] text-xl p-4 rounded">Enter</button>
       </form>
     </div>
   )
