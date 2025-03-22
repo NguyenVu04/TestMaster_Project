2efaf2778f9114c6506e4940b330bbee846cd08a
tonynin1 <viet.vonin1@hcmut.edu.vn>
2025-03-22 11:18:32 +0700

    next auth done

---
+++ frontend/app/utils/db.ts
@@ -0,0 +1,12 @@
+
+// function getUserFromDb
+export async function getUserFromDb(email: string, password: string) {
+    // Your logic to verify if the user exists
+    // Call your API from here
+    return {
+        userId: 1,
+        username: "John Doe",
+        email: "JohnDoe@example.com",
+        role: "admin",
+    }
+}
