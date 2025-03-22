40325059f9009a552f3771567c1bcf69cf26d8cf
anpn <anthachby2@gmail.com>
2025-03-22 15:21:20 +0700

    sign up teacher student

--- backend/src/main/java/project/testmaster/backend/controller/AuthController.java
+++ backend/src/main/java/project/testmaster/backend/controller/AuthController.java
@@ -7,18 +7,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import project.testmaster.backend.dto.StudentSignupRequest;
+import project.testmaster.backend.dto.SignupRequestDTO;
 import project.testmaster.backend.model.Student;
+import project.testmaster.backend.model.Teacher;
 import project.testmaster.backend.service.StudentService;
+import project.testmaster.backend.service.TeacherService;
 
 @RestController()
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private StudentService studentService;
+    @Autowired
+    private TeacherService teacherService;
 
     @PostMapping("/student/signup")
-    public ResponseEntity<Student> signup(@RequestBody StudentSignupRequest request) {
+    public ResponseEntity<Student> signupStudent(@RequestBody SignupRequestDTO request) {
         return ResponseEntity.ok(studentService.registerStudent(request));
+    }
+
+    @PostMapping("/teacher/signup")
+    public ResponseEntity<Teacher> signupTeacher(@RequestBody SignupRequestDTO request) {
+        return ResponseEntity.ok(teacherService.registerTeacher(request));
     }
 }
