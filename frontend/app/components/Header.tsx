8b9e3ddba99e8a787675ca354db024267d8f7559
Nguyễn Duy Vũ <132334654+NguyenVu04@users.noreply.github.com>
2025-03-29 12:20:47 +0700

    update

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -19,27 +19,27 @@ function Header() {
 
   const role = window.localStorage.getItem("role");
 
-  const session = useSession();
-  useEffect(() => {
-    console.log("Session:", session);
-    if (session.status === "loading") {
-      // Session is loading
-      console.log("Session is loading...");
-    } else if (session.status === "unauthenticated") {
-      // User is not authenticated
-      console.log("User is not authenticated");
-      return redirect("/auth/login");
-    } else if (session.status === "authenticated") {
-      console.log("User is authenticated: BUTTON");
-      setButton(
-        <Link className="inline-block w-full hover:text-gray-400" href="/">
-          Logout
-        </Link>
-      );
-      // User is authenticated
-      // console.log("User is authenticated:", session.data.user);
-    }
-  }, [session.status]);
+//   const session = useSession();
+//   useEffect(() => {
+//     console.log("Session:", session);
+//     if (session.status === "loading") {
+//       // Session is loading
+//       console.log("Session is loading...");
+//     } else if (session.status === "unauthenticated") {
+//       // User is not authenticated
+//       console.log("User is not authenticated");
+//       return redirect("/auth/login");
+//     } else if (session.status === "authenticated") {
+//       console.log("User is authenticated: BUTTON");
+//       setButton(
+//         <Link className="inline-block w-full hover:text-gray-400" href="/">
+//           Logout
+//         </Link>
+//       );
+//       // User is authenticated
+//       // console.log("User is authenticated:", session.data.user);
+//     }
+//   }, [session.status]);
 
   console.log(button);
 
